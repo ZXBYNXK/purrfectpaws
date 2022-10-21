9f596d2d20bcca49766cffa84e8866906d93698e
ZXBYNXK <tlbxvskizxbynxk@gmail.com>
2022-10-21 17:01:22 -0400

    Adding more default data and Footer element that will stick to every page.

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -20,6 +20,39 @@ const defaults = {
             text: "Anyone can be convinced that bad things won’t happen to them. The truth is accidents can and do happen. You can trust in us to handle these situations with grace and care."
         },
 
+    ],
+
+    services: [
+        {
+            title: "Pet Sitting",
+            text: "We come to YOUR home to pet sit! Our pet sitting service includes options for short visits, dog walking, live-in, and overnight care. We pet sit for almost all animals big and small. We also handle farm animals in some areas. Unfortunately we do not pet sit for snakes. ",
+            label: "Pet Sitting Rates",
+        },
+        {
+            title: "Doggy Daycare",
+            text: "Want to have your dog become more socialized while you’re at work? Drop them off at our doggy daycare!",
+            label: "Doggy Daycare Rates",
+        },
+        {
+            title: "Pooper Scooper",
+            text: "With this service, we come to your home and collect all the dog poop from your yard!",
+            label: "Pooper Scooper Rates",
+        },
+        {
+            title: "Boarding",
+            text: "If you’re traveling away for a few nights and would prefer to board your pet, come check out our home facility! (Available in AZ only) ",
+            label: "Boarding Rates",
+        },
+        {
+            title: "Pet Transportation",
+            text: "While Uber and Lyft drivers may not accept furry, scaly, or feathery passengers, we offer pet taxi services to help your family make trips to the groomers, vet, airport, and more! We can also do pick-up and drop-off when you board with us!",
+            label: "Pet Taxi Rates",
+        },
+        {
+            title: "Grooming",
+            text: "We can clip your pet’s toenails.",
+            label: "Grooming Rates"
+        }
     ]
 
 }
