0f47bca3e284406c6ce048da10105bbb0a3d7ff7
ZXBYNXK <tlbxvskizxbynxk@gmail.com>
2022-10-26 22:53:34 -0400

    Added functionality to let user know form has been sent or not.  Needs further additions for displaying errors in a user friendly way

--- src/components/forms/ContactUsForm.js
+++ src/components/forms/ContactUsForm.js
@@ -1,8 +1,11 @@
-import React, {useReducer, useState} from "react";
+import React, {useState} from "react";
+import api from "../../utils/api";
 import Button from "../layout/Button";
 import AboutYouForm from "./AboutYouForm";
 import ServiceRequestForm from "./ServiceRequestForm";
+
 const date = Date.now();
+
 const initialState = {
     firstName: '',
     lastName: '',
@@ -17,6 +20,9 @@ const initialState = {
     startDate: date,
     endDate: date,
     message: '',
+    submitted: false,
+    success: true,
+    errors: {}
 
 }
 
@@ -33,19 +39,43 @@ const ContactUsForm = () => {
         })
     }
     
-    const handleSubmit = (el) => {
+    const handleSubmit = async (el) => {
         el.preventDefault();
-        alert("Working...")
-        // dispatch({
-           // type: 'SUBMIT',
-        // })
+        try {
+            setFormData({...initialState, submitted: true, success: true})
+            let res = await api.post("/contact-us", formData);
+            console.log(res.body);
+        } catch (err) {
+            console.error(err);
+            setFormData({errors: err})
+        }
     }
 
     return (
-        <form className="form" onSubmit={handleSubmit} >
-            <AboutYouForm form={false} handleChange={handleChange} props={formData} />
-            <ServiceRequestForm form={false} handleChange={handleChange} props={formData} />
+        <form 
+            className="form" 
+            onSubmit={handleSubmit} 
+        >
+            <div className={`alert-box ${formData.success ? "alert-success" : "alert-danger"}`}>
+                {
+                    formData.submitted ? 
+                        (<h1>Form Sent.</h1>) : (<></>)
+                }
+            </div>
+            <AboutYouForm 
+                form={false} 
+                handleChange={handleChange} 
+                props={formData} 
+            />
+            
+            <ServiceRequestForm 
+                form={false} 
+                handleChange={handleChange} 
+                props={formData} 
+            />
+            
             <Button label="Submit"/>
+        
         </form>
     )
 }
--- src/components/forms/ServiceRequestForm.js
+++ src/components/forms/ServiceRequestForm.js
@@ -1,10 +1,18 @@
 import React from "react";
+
 const ServiceRequestForm = ({form=true, handleChange, props}) => {
+    
     const inputs = (            
         <div className="form-content">
+
             <h1>Service Request</h1>
+
             <span>Subject *</span> <br />
-            <select name="subject" onChange={handleChange} value={props.subject}>
+            <select 
+                name="subject" 
+                onChange={handleChange} 
+                value={props.subject}
+            >
                 <option value="Pet Sitting or Dog Walking">Pet Sitting or Dog Walking</option>
                 <option value="Pooper Scooper">Pooper Scooper</option>
                 <option value="Doggy Daycare">Doggy Daycare</option>
@@ -16,12 +24,31 @@ const ServiceRequestForm = ({form=true, handleChange, props}) => {
                 <option value="Other">Other</option>
             </select>
             <br />
+
             <span>Day of Service / Service Start Date</span> <br />
-            <input name="startDate" type="date" value={props.startDate} onChange={handleChange} /> <br />
+            <input 
+                name="startDate" 
+                type="date" 
+                value={props.startDate} 
+                onChange={handleChange}
+            /> <br />
+
             <span>Day of Service / Service End Date</span> <br />
-            <input name="endDate" type="date" value={props.endDate} onChange={handleChange} /> <br />
+            <input 
+                name="endDate" 
+                type="date" 
+                value={props.endDate} 
+                onChange={handleChange} 
+            /> <br />
+
             <span>Message / Comments</span><br />
-            <textarea name="message" type="text" value={props.message} onChange={handleChange}></textarea>
+            <textarea 
+                name="message" 
+                type="text" 
+                value={props.message} 
+                onChange={handleChange}
+            ></textarea>
+
         </div>
 );
     return form ? (
